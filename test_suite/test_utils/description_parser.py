53049a7a7bdadcf4b37e16dcfc933ea0b41cd93a
Aiden2244 <aidenm2244@gmail.com>
2024-09-12 02:32:21 -0400

    Added functionality to get console output in description_parser, added merge_and_sort() function

--- test_suite/test_utils/description_parser.py
+++ test_suite/test_utils/description_parser.py
@@ -7,16 +7,23 @@ class DescriptionParser:
     and context outputs from a file's content.
     """
     
+    """
+    regex pattern constants for identifying elements within the script
+    """
     __COMMAND_PATTERN = re.compile(r'\*\*Command:\*\*\s*\n```bash\s*\n(.*?)\n```', re.DOTALL)
-    __TEST_CASE_PATTERN = re.compile(r'## (TEST CASE) (\d+[a-zA-Z])', re.DOTALL)
-    __ERROR_CASE_PATTERN = re.compile(r'## (ERROR CASE) (\d+[a-zA-Z])', re.DOTALL)
+    __CONSOLE_OUTPUT_PATTERN = re.compile(r'\*\*Console output:\*\*\s*\n```bash\s*\n(.*?)\n```', re.DOTALL)
+    __TEST_CASE_PATTERN = re.compile(r'## (TEST CASE \d+[a-zA-Z])', re.DOTALL)
+    __ERROR_CASE_PATTERN = re.compile(r'## (ERROR CASE \d+[a-zA-Z])', re.DOTALL)
     __CONTEXT_OUTPUT_PATTERN = re.compile(r'\*\*context\.txt output:\*\*\s*```.*?\n(.*?)\n```', re.DOTALL)
 
+
     __commands = []
+    __console_outputs = []
     __test_cases = []
     __error_cases = []
     __context_outputs = []
     __description_text = ""
+
     
     def __get_line_number(self, position):
         return self.__description_text.count('\n', 0, position) + 1
@@ -31,6 +38,31 @@ class DescriptionParser:
 
         return parsed_outputs
 
+    # TODO: fix this
+    def __get_list_contents_as_str(self, list):
+        list_string = ""
+        for item in list:
+            list_string += f"Line {item[0]}: {item[1]}\n"
+        return list_string
+
+
+    def __str__(self):
+        desc = "COMMANDS: \n"
+        desc += self.__get_list_contents_as_str(self.__commands)
+
+        desc += "\nCONSOLE OUTPUTS: \n"
+        desc += self.__get_list_contents_as_str(self.__console_outputs)
+
+        desc += "\nTEST CASES:\n"
+        desc += self.__get_list_contents_as_str(self.__test_cases)
+
+        desc += "\nERROR CASES:\n"
+        desc += self.__get_list_contents_as_str(self.__error_cases)
+
+        desc += "\nCONTEXT.TXT OUTPUTS:\n"
+        desc += self.__get_list_contents_as_str(self.__context_outputs)
+
+        return desc
 
     def __init__(self, description_path):
         # read the description text in from the file
@@ -38,12 +70,17 @@ class DescriptionParser:
             self.__description_text = file.read()
 
         self.__commands = self.__get_list_from_matches(self.__COMMAND_PATTERN)
+        self.__console_outputs = self.__get_list_from_matches(self.__CONSOLE_OUTPUT_PATTERN)
         self.__test_cases = self.__get_list_from_matches(self.__TEST_CASE_PATTERN)
         self.__error_cases = self.__get_list_from_matches(self.__ERROR_CASE_PATTERN)
         self.__context_outputs = self.__get_list_from_matches(self.__CONTEXT_OUTPUT_PATTERN)
 
+
     def get_commands(self):
         return self.__commands
+
+    def get_console_output(self):
+        return self.__console_outputs
 
     def get_test_cases(self):
         return self.__test_cases
@@ -54,6 +91,25 @@ class DescriptionParser:
     def get_context_outputs(self):
         return self.__context_outputs
 
+
+    def merge_and_sort(self, lists, truncate=False):
+        """
+        Merge any number of lists from the parser and sort by line number.
+        If truncate is true, the merged list will contain only elements (false by default).
+        """
+        combined = []
+
+        if len(lists) < 2:
+            combined = lists
+        else:
+            for list in lists:
+                combined += list
+            combined = sorted(combined, key=lambda x: x[0])
+
+        if truncate: combined = [sublist[1] for sublist in combined]
+        return combined
+
+
 if __name__ == "__main__":
     """
     Main execution block to read file description text from the command line argument,
@@ -63,7 +119,5 @@ if __name__ == "__main__":
 
     parser = DescriptionParser(file_path)
 
-    print(f"Commands: {parser.get_commands()}\n")
-    print(f"Test cases: {parser.get_test_cases()}\n")
-    print(f"Error cases: {parser.get_error_cases()}\n")
-    print(f"Context outputs: {parser.get_context_outputs()}\n")
+    # print(parser)
+    print(parser.merge_and_sort([parser.get_commands(), parser.get_test_cases()], True))
